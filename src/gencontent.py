a88bee2d685fb02cbf126ea32f5e65598a8066ec
r3sh4p3 <reshape.kek@gmail.com>
2025-02-26 16:41:37 +0100

    added file generation for all content

--- src/gencontent.py
+++ src/gencontent.py
@@ -2,6 +2,16 @@ import os
 from markdown_blocks import markdown_to_html_node
 
 
+def generate_pages_recursive(dir_path_content, temlpate_path, dest_dir_path):
+    for root, dirs, files in os.walk(dir_path_content):
+        for file in files:
+            if file.endswith(".md"):
+                from_path = os.path.join(root, file)
+                relative_path = os.path.relpath(from_path, dir_path_content)
+                dest_path = os.path.join(dest_dir_path, relative_path)
+                dest_path = os.path.splitext(dest_path)[0] + ".html"
+                generate_page(from_path, temlpate_path, dest_path)
+
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
